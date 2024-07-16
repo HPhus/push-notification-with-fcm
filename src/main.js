617fc958c6c28a707da0e306c8dc304a8dc0c644
Van Chuong <134tutu431@gmail.com>
2024-07-16 15:01:59 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -16,21 +16,6 @@ throwIfMissing(process.env, [
   'SENSOR_COLLECTION_ID',
   'USERS_COLLECTION_ID',
 ]);
-// initailze firebase app
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FCM_PROJECT_ID,
-    clientEmail: process.env.FCM_CLIENT_EMAIL,
-    privateKey: process.env.FCM_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
-  databaseURL: process.env.FCM_DATABASE_URL,
-});
-
-const client = new Client()
-  .setEndpoint(process.env.APPWRITE_URL)
-  .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  .setKey(process.env.APPWRITE_API_KEY);
-const databases = new Databases(client);
 
 export default async ({ req, res, log, error }) => {
   try {
@@ -38,56 +23,47 @@ export default async ({ req, res, log, error }) => {
     const sensorCollectionID = process.env.SENSOR_COLLECTION_ID;
     const userCollectionID = process.env.USERS_COLLECTION_ID;
 
-    await databases.updateDocument(
-      '6682a4940008a5cdf763',
-      '6684f3c2000586689f8a',
-      '6684fa610035fa60c6d5',
-      {
-        battery: parseFloat('50'),
-      }
+    const users = await databases.listDocuments(
+      buildingDatabaseID,
+      userCollectionID,
+      [Query.limit(100000), Query.offset(0)]
+    );
+    const deviceTokens = users.documents.map((document) => document.deviceToken);
+
+    const promise = await databases.listDocuments(
+      buildingDatabaseID,
+      sensorCollectionID,
+      [Query.limit(100000), Query.offset(0)]
     );
 
-    // const users = await databases.listDocuments(
-    //   buildingDatabaseID,
-    //   userCollectionID,
-    //   [Query.limit(100000), Query.offset(0)]
-    // );
-    // const deviceTokens = users.documents.map((document) => document.deviceToken);
+    promise.documents.forEach(async (item) => {
+      const currentDate = new Date();
 
-    // const promise = await databases.listDocuments(
-    //   buildingDatabaseID,
-    //   sensorCollectionID,
-    //   [Query.limit(100000), Query.offset(0)]
-    // );
+      if (
+        item.value > 1000 &&
+        isMoreThan5MinutesAgo(item.lastNotification, currentDate)
+      ) {
+        // context.log('Sensor:' + item);
+        await sendPushNotification({
+          data: {
+            title: 'Cảnh báo cháy',
+            body:
+              'Thiết bị ' + item.name + ' đang ở mức độ cảnh báo cháy ('+item.value+")",
+            sensorId: item.$id,
+          },
+          tokens: deviceTokens,
+        });
 
-    // promise.documents.forEach(async (item) => {
-    //   const currentDate = new Date();
-
-    //   if (
-    //     item.value > 1000 &&
-    //     isMoreThan5MinutesAgo(item.lastNotification, currentDate)
-    //   ) {
-    //     // context.log('Sensor:' + item);
-    //     await sendPushNotification({
-    //       data: {
-    //         title: 'Cảnh báo cháy',
-    //         body:
-    //           'Thiết bị ' + item.name + ' đang ở mức độ cảnh báo cháy ('+item.value+")",
-    //         sensorId: item.$id,
-    //       },
-    //       tokens: deviceTokens,
-    //     });
-
-    //     await databases.updateDocument(
-    //       buildingDatabaseID,
-    //       sensorCollectionID,
-    //       item.$id,
-    //       {
-    //         lastNotification: currentDate,
-    //       }
-    //     );
-    //   }
-    // });
+        await databases.updateDocument(
+          buildingDatabaseID,
+          sensorCollectionID,
+          item.$id,
+          {
+            lastNotification: currentDate,
+          }
+        );
+      }
+    });
   } catch (e) {
     log(e);
   }
