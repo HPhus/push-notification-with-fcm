35d91e928241d8682898d2915f232648783fa3ba
Van Chuong <134tutu431@gmail.com>
2024-07-16 14:54:29 +0700

    test

--- src/main.js
+++ src/main.js
@@ -1,50 +1,85 @@
-import { throwIfMissing, sendPushNotification, index } from './utils.js';
+import { throwIfMissing, sendPushNotification, index,isMoreThan5MinutesAgo } from './utils.js';
 
 throwIfMissing(process.env, [
   'FCM_PROJECT_ID',
   'FCM_PRIVATE_KEY',
   'FCM_CLIENT_EMAIL',
-  'FCM_DATABASE_URL',
+  'APPWRITE_FUNCTION_PROJECT_ID',
+  'SENSOR_COLLECTION_ID',
+  'USERS_COLLECTION_ID',
 ]);
+// initailze firebase app
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FCM_PROJECT_ID,
+    clientEmail: process.env.FCM_CLIENT_EMAIL,
+    privateKey: process.env.FCM_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  databaseURL: process.env.FCM_DATABASE_URL,
+});
+
+const client = new Client()
+  .setEndpoint(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+  .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+  .setKey(process.env.APPWRITE_API_KEY);
+const databases = new Databases(client);
 
 export default async ({ req, res, log, error }) => {
-  // if (req.method == 'GET') {
-  //   await index();
-  //   return res.json({
-  //     message:
-  //       'Start testing the realtime read senor value and push notification function',
-  //   });
-  // }
-
-  // if (req.method == 'POST') {
-  //   try {
-  //     throwIfMissing(req.body, ['deviceToken', 'message']);
-  //     throwIfMissing(req.body.message, ['title', 'body']);
-  //   } catch (err) {
-  //     return res.json({ ok: false, error: err.message }, 400);
-  //   }
-
-  //   log(`Sending message to device: ${req.body.deviceToken}`);
-
-  //   try {
-  //     const response = await sendPushNotification({
-  //       data: {
-  //         title: req.body.message.title,
-  //         body: req.body.message.body,
-  //       },
-  //       tokens: req.body.deviceToken,
-  //     });
-
-  //     log(`Successfully sent message: ${response}`);
-
-  //     return res.json({ ok: true, messageId: response });
-  //   } catch (e) {
-  //     error(e);
-  //     return res.json({ ok: false, error: 'Failed to send the message' }, 500);
-  //   }
-  // }
   try {
-    await index();
+    const buildingDatabaseID = process.env.BUILDING_DATABASE_ID;
+    const sensorCollectionID = process.env.SENSOR_COLLECTION_ID;
+    const userCollectionID = process.env.USERS_COLLECTION_ID;
+
+    await databases.updateDocument(
+      "6682a4940008a5cdf763",
+      "6684f3c2000586689f8a",
+      "6684fa610035fa60c6d5",
+      {
+        battery: parseFloat("50"),
+      }
+    );
+
+    // const users = await databases.listDocuments(
+    //   buildingDatabaseID,
+    //   userCollectionID,
+    //   [Query.limit(100000), Query.offset(0)]
+    // );
+    // const deviceTokens = users.documents.map((document) => document.deviceToken);
+
+    // const promise = await databases.listDocuments(
+    //   buildingDatabaseID,
+    //   sensorCollectionID,
+    //   [Query.limit(100000), Query.offset(0)]
+    // );
+
+    // promise.documents.forEach(async (item) => {
+    //   const currentDate = new Date();
+
+    //   if (
+    //     item.value > 1000 &&
+    //     isMoreThan5MinutesAgo(item.lastNotification, currentDate)
+    //   ) {
+    //     // context.log('Sensor:' + item);
+    //     await sendPushNotification({
+    //       data: {
+    //         title: 'Cảnh báo cháy',
+    //         body:
+    //           'Thiết bị ' + item.name + ' đang ở mức độ cảnh báo cháy ('+item.value+")",
+    //         sensorId: item.$id,
+    //       },
+    //       tokens: deviceTokens,
+    //     });
+
+    //     await databases.updateDocument(
+    //       buildingDatabaseID,
+    //       sensorCollectionID,
+    //       item.$id,
+    //       {
+    //         lastNotification: currentDate,
+    //       }
+    //     );
+    //   }
+    // });
   } catch (e) {
     log(e);
   }
--- src/utils.js
+++ src/utils.js
@@ -8,78 +8,11 @@ throwIfMissing(process.env, [
   'FCM_DATABASE_URL',
 ]);
 
-// initailze firebase app
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FCM_PROJECT_ID,
-    clientEmail: process.env.FCM_CLIENT_EMAIL,
-    privateKey: process.env.FCM_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
-  databaseURL: process.env.FCM_DATABASE_URL,
-});
 
-const client = new Client()
-  .setEndpoint(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  .setKey(process.env.APPWRITE_API_KEY);
-const databases = new Databases(client);
 
 export async function index() {
   try {
-    const buildingDatabaseID = process.env.BUILDING_DATABASE_ID;
-    const sensorCollectionID = process.env.SENSOR_COLLECTION_ID;
-    const userCollectionID = process.env.USERS_COLLECTION_ID;
-
-    await databases.updateDocument(
-      "6682a4940008a5cdf763",
-      "6684f3c2000586689f8a",
-      "6684fa610035fa60c6d5",
-      {
-        battery: parseFloat("50"),
-      }
-    );
-
-    const users = await databases.listDocuments(
-      buildingDatabaseID,
-      userCollectionID,
-      [Query.limit(100000), Query.offset(0)]
-    );
-    const deviceTokens = users.documents.map((document) => document.deviceToken);
-
-    const promise = await databases.listDocuments(
-      buildingDatabaseID,
-      sensorCollectionID,
-      [Query.limit(100000), Query.offset(0)]
-    );
-
-    promise.documents.forEach(async (item) => {
-      const currentDate = new Date();
-
-      if (
-        item.value > 1000 &&
-        isMoreThan5MinutesAgo(item.lastNotification, currentDate)
-      ) {
-        // context.log('Sensor:' + item);
-        await sendPushNotification({
-          data: {
-            title: 'Cảnh báo cháy',
-            body:
-              'Thiết bị ' + item.name + ' đang ở mức độ cảnh báo cháy ('+item.value+")",
-            sensorId: item.$id,
-          },
-          tokens: deviceTokens,
-        });
-
-        await databases.updateDocument(
-          buildingDatabaseID,
-          sensorCollectionID,
-          item.$id,
-          {
-            lastNotification: currentDate,
-          }
-        );
-      }
-    });
+    
   } catch (e) {
     // context.log('Read sensor error:' + e);
   }
